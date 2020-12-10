fdabf6cc66089b7032218e0b4e86c0f87fab1589
jesse berkowitz <jberkow@brandeis.edu>
2020-12-10 13:05:40 -0500

    a

--- UpgradedChatbot.py
+++ UpgradedChatbot.py
@@ -6,6 +6,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 import numpy as np
 import warnings
+import re  
 warnings.filterwarnings('ignore')
 
 nltk.download('punkt', quiet=True)
@@ -14,10 +15,12 @@ article.download()
 article.parse()
 article.nlp()
 corpus = article.text
+
 # print(corpus)
 
 text = corpus
 sentence_list = nltk.sent_tokenize(text)
+
 #Got sentences in list from this specific article on Kidney issues
 # print(sentence_list)
 
@@ -58,7 +61,8 @@ def bot_response(user_input):
     similarity_scores = cosine_similarity(cm[-1], cm)
     similarity_scores_list = similarity_scores.flatten()
     index = index_sort(similarity_scores_list)
-    index = index[1:]
+    length_user_input = len(user_input)
+    index = index[length_user_input:]
     response_flag = 0
     
     j = 0
@@ -116,29 +120,72 @@ def tokenize(article):
     tokenizer = RegexpTokenizer(r'\w+')
     for word in Words2:
         tokenizer.tokenize(word)
-        Words.append(word)
+        if word not in Words:
+            Words.append(word)
 
 
     return(Words)
     
+def smarter_bot_response(user_input, article):
+
+    user_input = user_input.lower()
+    word_list = tokenize(article)
+    # word_list gives us a list of words from the entire article
+    # we want to compare each word in the user_input with words in the article to find highest correlated word
+    # we want the bot to then create sentences piecing together words with highest correlation to highest correlated word
+
+    #in order for this to work, we need to take the words in this list, and create sentences from them
+    # in a way that first makes some sense, but also, puts correlated words together
+
+    word_list.append(user_input)
+    bot_response = ''
+    cm = CountVectorizer().fit_transform(word_list)
+    #comparing last user_input to overall matrix
+    similarity_scores = cosine_similarity(cm[-1], cm)
+    similarity_scores_list = similarity_scores.flatten()
+    index = index_sort(similarity_scores_list)
+    #make sure it can not mimic the user's words
+    length_user_input = len(user_input)
+
+
+    
+    index = index[2:]
+    response_flag = 0
+    
+    j = 0
+    for i in range(len(index)):
+        if similarity_scores_list[index[i]] > 0.0:
+            bot_response = bot_response + ' ' + word_list[index[i]]
+            response_flag = 1
+            break
+        
+    if response_flag == 0:
+
+        #didnt find similar sentence:
+        # 
+        bot_response = bot_response + ' ' + "Sorry, I could not find what you're looking for."     
+    word_list.remove(user_input)
+    return bot_response    
     
     
-    
 
 
 
-print(tokenize(article))
+# a = (tokenize(article))
+# cm = CountVectorizer().fit_transform(a)
+# print(cm)
 
-# print("I am Doc_Bot: I am here to help you learn about Eddie Van Halen")
-# print("What do you want to know?")
-# exit_list = ['exit', 'see you later', 'bye', 'quit']
-# while(True):
-#     user_input = input()
-#     if user_input in exit_list:
-#         print("See you later")
-#         break
-#     else:
-#         if greeting_response(user_input) != None:
-#             print("Doc Bot:" + greeting_response(user_input))
-#         else:
-#             print('Doc Bot:' + bot_response(user_input))    
+
+print("I am Doc_Bot: I am here to help you learn about Eddie Van Halen")
+print("What do you want to know?")
+exit_list = ['exit', 'see you later', 'bye', 'quit']
+while(True):
+    user_input = input()
+    if user_input in exit_list:
+        print("See you later")
+        break
+    else:
+        if greeting_response(user_input) != None:
+            print("Doc Bot:" + greeting_response(user_input))
+        else:
+            print('Doc Bot:' + bot_response(user_input))    
